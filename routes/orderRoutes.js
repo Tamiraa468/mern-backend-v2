b1ce8aa34b17213181c9f930f46a85658dba51d3
Tamir Udelgombo <udelgombotamira@gmail.com>
2025-05-27 23:45:59 +0800

    ec2

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -15,5 +15,5 @@ router.route('/mine').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
-
+// hhh
 export default router;
